079b9cba62423c87e68348f06d5dc8ab8080b8e5
chaiwat Sombatthepsut <ice@appsynth.net>
2019-01-22 10:41:56 +0700

    Initial Commit

---
+++ WeatherApp-MVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp-MVVM
+//
+//  Created by Ice on 22/1/2562 BE.
+//  Copyright © 2562 Ice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
